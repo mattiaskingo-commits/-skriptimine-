3cd65b3cb72d15964cfa92d9aefef65491bae518
Mattias Kingo <mattias.kingo@voco.ee>
2026-09-04 04:01:14 -0400

    kuupaeva ja kalendri valjastamine

---
+++ praks2/yl3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Skript valjastab tanase kuupaeva, kellaaja ja kuu kalendri
+
+echo "Today is $(date +"%A, %B %d, %Y")"
+echo "Time is $(date +"%H:%M")"
+
+echo ""
+echo "Calendar of $(date +"%B %Y")"
+
