aa8648fd65df748d1486dd013d1a285f61ec10bf
Mattias Kingo <mattias.kingo@voco.ee>
2026-09-04 06:46:37 -0400

    kasutaja taisnime lugemine ja tervitus

---
+++ praks3/yl1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Skript kysib kasutaja taisnime ja tervitab teda
+
+ echo -n "Sisesta oma ees- ja perenimi: "
+read taisnimi
+echo "Tere tulemast, $taisnimi"
+
+ echo -n "Sisesta oma ees- ja perenimi: "
+read eesnimi perenimi 
+echo "Sinu eesnimi on $eesnimi ja perenimi on $perenimi"
