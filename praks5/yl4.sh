1cb4ff21f16269ced36b2b18a599fba5180e7574
Mattias Kingo <mattias.kingo@voco.ee>
2026-09-11 05:13:32 -0400

    automaatne ajapohine tervitus kasutaja nimega

---
+++ praks5/yl4.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# aja jargi tervitamine koos kasutaja nimega, aeg loetakse susteemist
+#
+aeg=$(date +"%H")
+# eemaldame algusnulli (nt 08 -> 8), muidu bash tolgendab seda vale numbrina
+aeg=$((10#$aeg))
+nimi=$(whoami)
+nimi="${nimi^}" 
+#
+if [ $aeg -ge 6 -a $aeg -lt 12 ]
+then
+echo "Tere hommikust, $nimi!"
+elif [ $aeg -ge 12 -a $aeg -lt 18 ]
+then
+echo "Tere paevast, $nimi!"
+elif [ $aeg -ge 18 -a $aeg -lt 22 ]
+then
+echo "Tere ohtust, $nimi!"
+elif [ $aeg -ge 22 -a $aeg -lt 24 ] || [ $aeg -ge 0 -a $aeg -lt 6 ] 
+then
+echo "Head ood, $nimi!"
+else
+echo "Vale sisend"
+fi
+
